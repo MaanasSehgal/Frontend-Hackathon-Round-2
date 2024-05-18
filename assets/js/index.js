8f9524aa57e0b14e22a6e1c55ec4b906897c67c5
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-05-18 21:49:16 +0530

    Completed sustainability and diversity page with all images and url links

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,20 @@
+const cursorDot = document.querySelector(".cursor-dot");
+const cursorOutline = document.querySelector(".cursor-outline");
+window.addEventListener("mousemove", function (e) {
+    const posX = e.clientX;
+    const posY = e.clientY;
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+    cursorOutline.animate(
+        {
+            left: `${posX}px`,
+            top: `${posY}px`,
+        },
+        //control speed of outline reaching cursor
+        {duration: 150, fill: "forwards"}
+    );
+});
+
 // Function to fetch news for a specific year
 function fetchNews(year) {
     // Dummy data for demonstration

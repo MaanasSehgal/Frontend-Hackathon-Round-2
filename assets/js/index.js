0e927ba6d3322014703e366becefb030b22ebfa0
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-05-19 10:48:17 +0530

    Removed cursor, improved home page ui, minor fixes in bugs

--- assets/js/index.js
+++ assets/js/index.js
@@ -96,24 +96,6 @@ $(function () {
     });
 });
 
-const cursorDot = document.querySelector(".cursor-dot");
-const cursorOutline = document.querySelector(".cursor-outline");
-
-window.addEventListener("mousemove", function (e) {
-    const posX = e.clientX;
-    const posY = e.clientY;
-    cursorDot.style.left = `${posX}px`;
-    cursorDot.style.top = `${posY}px`;
-    cursorOutline.animate(
-        {
-            left: `${posX}px`,
-            top: `${posY}px`,
-        },
-        //control speed of outline reaching cursor
-        {duration: 150, fill: "forwards"}
-    );
-});
-
 // document.addEventListener("DOMContentLoaded", function () {
 //     gsap.registerPlugin(ScrollTrigger);
 
